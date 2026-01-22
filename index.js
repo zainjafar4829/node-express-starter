a72049ffdc96aad0a9e69abbac31ee8c71d618f1
zain jafar <zain.jaffar@algorepublic.com>
2026-01-22 14:28:31 +0500

    changes in response middleware and add security middlewares

--- index.js
+++ index.js
@@ -3,25 +3,36 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const compression = require("compression");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const multer = require("multer");
 const connectDB = require("./config/db");
 
 const appRoutes = require("./routes/index");
-const errorHandler = require("./middlewares/error");
-const responseHandler = require("./middlewares/response");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require("path");
 const upload = multer();
 
 // Middleware
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
 app.use(compression());
-app.use(responseHandler);
 app.use(upload.any());
+//  this is used for some static files that are stored on the server
+app.use("/storage", express.static(path.join(__dirname, "public")));
 // Routes
 app.use("/api/v1", appRoutes);
 
@@ -33,8 +44,6 @@ app.get("/", (req, res) => {
     timestamp: new Date().toISOString(),
   });
 });
-
-app.use(errorHandler);
 
 // Start Server
 const startServer = async () => {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express.Router();
 const authRouter = require("./auth");
+const responseHandler = require("../middlewares/response");
 
 app.use("/auth", authRouter);
+app.use(responseHandler);
 module.exports = app;
